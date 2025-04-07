a7d55496bacf95e8c5f22256fe92911367bdba60
Hassan <50418905+mhassxx@users.noreply.github.com>
2025-04-08 00:17:05 +0100

    IN PROGRESS ADDED WORKING REGISTER. NOW TO COOKIES AND LOGIN LOGOUT

---
+++ HealthApp/Controllers/LoginController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HealthApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+            
+
+
+    }
+}
---
+++ HealthApp/Controllers/RegisterController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using HealthApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace HealthApp.Controllers
+{
+    public class RegisterController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public RegisterController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new Users
+                {
+                    Name = model.Name,
+                    Password = model.Password, // Consider hashing this in production
+                    Email = model.Email
+                };
+
+                _context.Users.Add(user);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Index", "Home"); // or wherever you want to go after registering
+            }
+
+            return View(model);
+        }
+
+
+
+
+
+    }
+}
