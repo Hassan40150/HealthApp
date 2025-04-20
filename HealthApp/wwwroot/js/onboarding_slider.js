72408a0488ebe1140e11fc78bcede73797697b86
Hassan <50418905+mhassxx@users.noreply.github.com>
2025-04-20 04:44:37 +0100

    ONBOARDING HEIGHT DONE

---
+++ HealthApp/wwwroot/js/onboarding_slider.js
@@ -0,0 +1,13 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+	const sliders = document.querySelectorAll(".styled-slider");
+
+	function updateSliderFill(slider) {
+		const percent = ((slider.value - slider.min) / (slider.max - slider.min)) * 100;
+		slider.style.background = `linear-gradient(to right, #49BDF8 0%, #49BDF8 ${percent}%, #fff ${percent}%, #fff 100%)`;
+	}
+
+	sliders.forEach(slider => {
+		updateSliderFill(slider); // initial fill
+		slider.addEventListener("input", () => updateSliderFill(slider));
+	});
+});
