14627bfeefcdfd860566acccf5e35400a53189f9
Hassan <50418905+mhassxx@users.noreply.github.com>
2025-04-25 16:03:52 +0100

    ADDED DBS STREAKS JOURNAL

---
+++ HealthApp/Models/Journal.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HealthApp.Models
+{
+    public class Journal
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public DateTime Timestamp { get; set; }
+
+        public required string Entry { get; set; }
+
+        [ForeignKey("User")]
+        public int UserID { get; set; }
+    }
+}
